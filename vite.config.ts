f45b4a341cce1cfc4a3468a709e9a7dfe21be2c2
adeyemi adekunle <adeyemisuraju15@gmail.com>
2024-10-25 20:43:54 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   plugins: [react(),
-    basicSsl()
+    // basicSsl()
   ],
   resolve: {
     alias: {
